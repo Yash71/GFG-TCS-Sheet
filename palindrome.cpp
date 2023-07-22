d55196b2c42c4641a65187139fe37c81db3225f8
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2023-07-23 00:08:26 +0530

    checking palindrome string

---
+++ palindrome.cpp
@@ -0,0 +1,12 @@
+// problem link : https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+
+int isPalindrome(string S)
+	{   
+	    int i=0,j=S.length()-1;
+	    while(i<j){
+	        if(S[i++]!=S[j--]){
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
