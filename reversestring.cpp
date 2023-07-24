5ef4f26ae17f80a9605258db11ca0267b79f2560
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2023-07-24 15:03:04 +0530

    reverse the string

---
+++ reversestring.cpp
@@ -0,0 +1,8 @@
+// problem link: https://practice.geeksforgeeks.org/problems/reverse-a-string/1
+string reverseWord(string str){
+    int i=0,j=str.length()-1;
+    while(i<=j){
+        swap(str[i++],str[j--]);
+    }
+    return str;
+}
