5431c5cb8bdd0e91dcebf318e5d84e6e2637834a
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2023-07-27 23:50:04 +0530

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,22 @@
+int shouldPunish (int roll[], int marks[], int n, double avg)
+    {
+       int sumMarks=0;
+       int swaps=0;
+       for(int i=0;i<n;i++)
+           sumMarks+=marks[i];
+       for(int i=0;i<n-1;i++)
+       {
+           for(int j=0;j<n-i-1;j++)
+           {
+               if(roll[j]>roll[j+1])
+               {
+                   swaps++;
+                   int temp=roll[j];
+                   roll[j]=roll[j+1];
+                   roll[j+1]=temp;
+               }
+           }
+       }
+       int fakeAvg=(sumMarks-swaps)/n;
+       return fakeAvg>=avg?1:0;
+    }
